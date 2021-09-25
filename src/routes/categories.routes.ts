f500111d80313e60fd1f6bac2ddddac4fbe5ef5c
Luiz Henrique <7henrique18@gmail.com>
2021-09-25 12:58:07 -0300

    feat: working with upload (csv parse)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,30 +1,27 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
-import { CategoriesRepository } from '../repositories/CategoriesRepositories';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import multer from 'multer';
+
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRoutes = Router();
 
-const categoriesRepository = new CategoriesRepository();
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  try {
-    createCategoryService.execute({ name, description });
-  } catch (err) {
-    return response.status(400).send({ error: `${err}` });
-  }
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const all = categoriesRepository.list();
+  return listCategoriesController.handle(request, response);
+});
 
-  return response.json({ all });
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
